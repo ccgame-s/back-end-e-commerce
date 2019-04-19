5676f639ab173d61328ecd782ed4d5a636c91c96
newsear <zyncgx@hotmail.com>
2019-04-19 22:09:07 +0700

    init with hello world

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express()
+dotenv.config()
+
+const { PORT } = process.env
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!')
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
