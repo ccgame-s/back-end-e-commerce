d02d38380350f15f7e7432f0b4170dcd7a106b69
newsear <zyncgx@hotmail.com>
2019-05-18 16:20:44 +0700

    allow all header

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const requireJwt = require('../libs/jwt')
 
 router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', '*')
   next()
 })
 
