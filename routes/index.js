e3b73e057a2d5766b3b8411c58b33aab5f87e613
newsear <zyncgx@hotmail.com>
2019-05-18 18:43:03 +0700

    use cors instead of allow bra bra bra

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 const router = require('express').Router()
+const Cors = require('cors')
 
 const auth = require('./auth')
 const products = require('./products')
 
 const requireJwt = require('../libs/jwt')
 
-router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
-})
+router.use(Cors())
 
 router.use('/', auth)
 router.use('/products', requireJwt, products)
